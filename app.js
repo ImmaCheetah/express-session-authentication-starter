26e0ae476ebf4745f9ad07cc49cd2bc428cf4b2d
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2024-10-24 11:40:27 -0400

    feat: convert mongo to postgres

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
+// Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
+require('dotenv').config();
+
 const express = require('express');
-const mongoose = require('mongoose');
 const session = require('express-session');
-var passport = require('passport');
-var crypto = require('crypto');
-var routes = require('./routes');
+const passport = require('passport');
+const crypto = require('crypto');
+const routes = require('./routes');
 const connection = require('./config/database');
 
 // Package documentation - https://www.npmjs.com/package/connect-mongo
-const MongoStore = require('connect-mongo')(session);
+const pgStore = require('connect-pg-simple')(session);
 
 // Need to require the entire Passport config module so app.js knows about it
 require('./config/passport');
 
 /**
  * -------------- GENERAL SETUP ----------------
- */
-
-// Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
-require('dotenv').config();
-
+*/
 // Create the Express application
-var app = express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-
 /**
  * -------------- SESSION SETUP ----------------
- */
+*/
+const sessionStore = new pgStore({pool: connection})
 
-// TODO
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false, 
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 // 1 day
+    }
+}))
 
 /**
  * -------------- PASSPORT AUTHENTICATION ----------------
@@ -39,7 +45,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 /**
  * -------------- ROUTES ----------------
  */
@@ -47,10 +52,9 @@ app.use(passport.session());
 // Imports all of the routes from ./routes/index.js
 app.use(routes);
 
-
 /**
  * -------------- SERVER ----------------
  */
 
 // Server listens on http://localhost:3000
-app.listen(3000);
+app.listen(3000, () => console.log("app listening on port 3000!"));
